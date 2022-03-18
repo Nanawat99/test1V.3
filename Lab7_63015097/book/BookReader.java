4c087a97d4b2e034134817f5643f79de5a5bcb9f
Nanawat99 <88593283+Nanawat99@users.noreply.github.com>
2022-03-18 17:24:37 +0700

    Add files via upload

---
+++ Lab7_63015097/book/BookReader.java
@@ -0,0 +1,23 @@
+package com.solid.book;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class BookReader implements Reader{
+
+    public static void main(String[] args) {
+
+        BookReader bookreader = new BookReader();
+        bookreader.printToScreen();
+    }
+
+    @Override
+    public void printToScreen() {
+        Book book = new Book("Book", List.of("Chapter 1", "Chapter 2", "Chapter 3", "Chapter 4", "Chapter 5"));
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
+}
